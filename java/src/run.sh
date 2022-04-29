3db1d4e5719827af38c7f2a47b35347f1bd777d7
wagerc97 <wager.clemens@gmail.com>
2022-04-29 15:25:03 +0200

    commit run.sh runs, dirs good

--- java/src/run.sh
+++ java/src/run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-exec javac /var/src/Main.java
-exec java -jar /var/src/Main.jar
+#exec cd /var/src/
+#exec javac Main.java
+#exec java -jar Main.jar
+
+#project_dir="/var/java/src"
+#cd $project_dir
+echo "Compiling Test project.."
+#if [ javac Main.java ] && [ javac DatabasHelper.java ] && [ jvavac TestConnection.java ] && [ javac HelloWorld.java ];
+if [ javac HelloWorld.java ];
+then
+   echo "Compilation successful! Running project Test"
+   java HelloWorld
+else
+   echo "Compilation failed. Cannot run project"
+fi
