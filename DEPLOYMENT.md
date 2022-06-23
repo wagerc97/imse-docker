4659218b62658207f4f1c517169e7f724dc6aced
wagerc97 <wager.clemens@gmail.com>
2022-06-23 12:27:31 +0200

    working on table create automation and exceptions

--- DEPLOYMENT.md
+++ DEPLOYMENT.md
@@ -2,7 +2,7 @@
 ## Start Docker container from local repository ###
 1. Navigate to whole project folder where docker-compose-local.yml is
 2. Build and start container <br>
-   ``docker-compose -f docker-compose-local.yml up``
+   ``docker-compose -f docker-compose-local.yml up --build``
 
  
 Resulting locally hosed [Webapplication](http://127.0.0.1:8000/)
@@ -11,7 +11,7 @@ Resulting locally hosed [Webapplication](http://127.0.0.1:8000/)
 #### See: https://docs.docker.com/engine/reference/commandline/build/#git-repositories
 1. Navigate to project folder where docker-compose-remote.yml is
 2. Run the following command in CLI <br>
-   ``docker-compose -f docker-compose-remote.yml up``
+   ``docker-compose -f docker-compose-remote.yml up --build``
 
 
 Resulting locally hosed [Webapplication](http://127.0.0.1:8000/)
@@ -25,120 +25,12 @@ On Windows 10-Home OS
   ``docker login`` <br> and enter your credientials 
 - download [docker-compose-remote.yml](compose/oraclesql-comp/docker-compose-remote.yml) from [GitHub directory](https://github.com/wagerc97/imse-docker/tree/master/compose)
 - navigate CLI to yml file and compose container <br>
-  ``docker-compose -f docker-compose-remote.yml up``
+  ``docker-compose -f docker-compose-remote.yml up --build``
 - Resulting locally hosed [Webapplication](http://127.0.0.1:8000/)
 
 ---
 ## Check system commands
-### Oracle SQL
 
-1. open terminal
-2. type ``docker ps`` to check which containers are running right now
-3. type either  
-   `` docker exec -it oraclesql8 bash``  
-      you are now in the db-docker (...sql8 is the container name)
-4. Check if listeners are up and running  
-  ``lsnrctl status`` -> bash: lsnrctl: command not found
-6. Navigate with sqlplus --> following: https://docs.oracle.com/cd/A97630_01/win.920/a95490/username.html   
-   1. To starts SQL*Plus without a database connection: ``sqlplus /NOLOG``  
-
-Tried to fix the ORA-12162 error: http://www.dba-oracle.com/t_ora_12162_tns_net_service_name.htm
-> ORACLE_HOME=/u01/oracle; export ORACLE_HOME  
-> ORACLE_SID=asdb; export ORACLE_SID
-
-Cannot connect though:
-sqlplus connect sys as sysdba -> 
-#
-#
-> sqlplus /nolog  
->> enter SQL cli 
-
-> sqlplus devuser/devpass@IMSEDB 
->> really nothing happens...no commands work anymore
-
-> **SQL>** connect sys as sysdba  
->> Enter password: ...?
-
-> **SQL>** SELECT TABLESPACE_NAME FROM USER_TABLESPACES;  
-> Oracle uses shemas and not databases like mysql.
->> SP2-0640: Not connected  
->> -> because "The Oracle Listener process was not successfully created.
-This error indicates that the TNSNAMES.ora file was not created during the installation process.
-"
-
-
-5. Some commands to look through the DBMS:   
-   If the tables are filled, the DB setup was successful!  
-   1. To enter sqlplus CLI: ``sqlplus /NOLOG``
-
----
-``[root@e4eb67d8fa54 lib]# ls``
->glogin.sql         libclntsh.so.11.1  libclntsh.so.19.1      libipc1.so   libocci.so.19.1  liboramysql19.so  network         xstreams.jar
-libclntsh.so       libclntsh.so.12.1  libclntshcore.so       libmql1.so   libociei.so      libsqlplus.so     ojdbc8.jar
-libclntsh.so.10.1  libclntsh.so.18.1  libclntshcore.so.19.1  libnnz19.so  libocijdbc19.so  libsqlplusic.so   ottclasses.zip
-
-``[root@e4eb67d8fa54 lib]# pwd``
->/lib/oracle/19.15/client64/lib
-
-
-***********************************************************************  
-
->Fatal NI connect error 12545, connecting to:
-(DESCRIPTION=(ADDRESS=(PROTOCOL=beq)(PROGRAM=/u01/oracle/product/imse_sql_db/bin/oracle)(ARGV0=oracleorcl)(ARGS='(DESCRIPTION=(LOCAL=YES)(ADDRESS=(PROTOCOL=beq)))')(DETAC
-H=NO))(CONNECT_DATA=(CID=(PROGRAM=sqlplus)(HOST=e4eb67d8fa54)(USER=root))))
-
->VERSION INFORMATION:
-TNS for Linux: Version 19.0.0.0.0 - Production
-Oracle Bequeath NT Protocol Adapter for Linux: Version 19.0.0.0.0 - Production
-TCP/IP NT Protocol Adapter for Linux: Version 19.0.0.0.0 - Production
-Version 19.15.0.0.0
-Time: 05-JUN-2022 19:10:48
-Tracing not turned on.
-Tns error struct:
-ns main err code: 12545
-
->TNS-12545: Message 12545 not found; No message file for product=network, facility=TNS
-ns secondary err code: 12560
-nt main err code: 515
-
->TNS-00515: Message 515 not found; No message file for product=network, facility=TNS
-nt secondary err code: 2
-nt OS err code: 0
-
-
-``[root@e4eb67d8fa54 trace]# pwd``  
->/root/oradiag_root/diag/clients/user_root/host_4135610220_110/trace  
-
-
-``[root@e4eb67d8fa54 admin]# more README``
->============================================================================
-This is the default directory for Oracle Network and Oracle Client
-configuration files. You can place files such as tnsnames.ora, sqlnet.ora
-and oraaccess.xml in this directory.
-NOTE:
-If you set an environment variable TNS_ADMIN to another directory containing
-configuration files, they will be used instead of the files in this default
-directory.
-============================================================================
-
-``[root@e4eb67d8fa54 admin]# pwd``
->/usr/lib/oracle/19.15/client64/lib/network/admin
-
-
-
-
-
-
-
-
-
-
-
-
-
----
----
----
 
 ### MYSQL
 
--- python/db_import/db_import.py
+++ python/db_import/db_import.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Wait for docker network to eventually setup 
-#time.sleep(30) # wait for db to be up
+time.sleep(10) # wait for db to be up
               # could be fancier if we tried to connect to the databse via tcp
               # and kept retrying until the connection doesn't get dropped anymore.
 
@@ -25,17 +25,31 @@ logging.basicConfig(level=logging.INFO)
 
 # the database connection
 connection_string = os.environ.get('DB_URI', 'sqlite:////data/database.sqlite')
-
 sqlengine = sa.create_engine(connection_string) 
+meta = sa.MetaData() # holds all db information
 
-
+autoIncrementScore = 1
 
 
 #################
 # Create TABLES #
 #################
 
-meta = sa.MetaData() # holds all db information
+tablesToCreate = ["Region", "Country", "Client", "Product", "Campaign", "Employee", "Marketing_emp", "Advertises", "General_Manager", "Orders"]
+# https://stackoverflow.com/questions/33053241/sqlalchemy-if-table-does-not-exist
+#allTableCreations = [meta.tables.keys()]
+for t in meta.tables.keys():  # traverse over all tables in metaData
+    if not sqlengine.dialect.has_table(sqlengine, tablesToCreate[]):  # If table don't exist, Create.
+        meta = sa.MetaData(sqlengine)
+
+        # Create a table with the appropriate Columns
+        Table(Variable_tableName, metadata,
+            Column('Id', Integer, primary_key=True, nullable=False), 
+            Column('Date', Date), Column('Country', String),
+            Column('Brand', String), Column('Price', Float))
+        # Implement the creation
+meta.create_all()
+
 
 # Build tables
 tableT = sa.Table( 'T', meta,
@@ -45,22 +59,21 @@ tableT = sa.Table( 'T', meta,
     sa.Column("c",  sa.Text()),
 )
 
-tableClient = sa.Table('Client', meta,
-    sa.Column("ID_client",      sa.Integer(),  primary_key=True), 
-    sa.Column("Client_Name",    sa.String(50), nullable=False), 
-    sa.Column("Country_Name",   sa.String(25), nullable=False) 
-)
-
-#tableClient.create(sqlengine)
-
 tableRegion = sa.Table('Region', meta,
     sa.Column("ID_region",     sa.Integer(),  primary_key=True),  # auto increment ?
     sa.Column("Region_Name",   sa.String(25), nullable=False)
 )
+#tables_dict.add(tableRegion:autoIncrementScore)
 
-tableCountry = Sa.Table('Country', meta,
+tableCountry = sa.Table('Country', meta,
     sa.Column("Country_Name",  sa.String(25), primary_key=True),  # auto increment ?
     sa.Column("ID_region",     sa.Integer(),  sa.ForeignKey("Region.ID_region"))
+)
+
+tableClient = sa.Table('Client', meta,
+    sa.Column("ID_client",      sa.Integer(),  primary_key=True), 
+    sa.Column("Client_Name",    sa.String(50), nullable=False), 
+    sa.Column("Country_Name",   sa.String(25), nullable=False) 
 )
 
 
@@ -71,48 +84,98 @@ meta.create_all(sqlengine)
 # connect to database 
 db_connection = sqlengine.connect()
 
+time.sleep(5)
+
 # https://www.pythonsheets.com/notes/python-sqlalchemy.html 
+
+
+
+
+##############################
+# Database HELPER functions  #
+##############################
+
+# temporary --> define with tables above
+tables_ID_dict = { #key=table : value=autoIncrementScore
+    tableRegion : autoIncrementScore, 
+    tableCountry : autoIncrementScore,
+    tableClient : autoIncrementScore,
+    "Product" : autoIncrementScore,
+    "Campaign" : autoIncrementScore,
+    "Employee" : autoIncrementScore,
+    "Marketing_emp" : autoIncrementScore,
+    "Advertises" : autoIncrementScore,
+    "General_Manager" : autoIncrementScore,
+    "Orders" : autoIncrementScore
+    }
+
+
+
+def autoIncrement(tableName):
+    autoIncrementScore = tables_ID_dict[tableName] # store current value
+    tables_ID_dict[tableName] += 1
+    return autoIncrementScore
+    
+
 
 
 
 ###############################
 # Database INSERT statements  #
 ###############################
+"""
+# order of insertion #
+Region
+Country
+Client
+Product
+Campaign
+Employee
+Marketing_emp
+Advertises
+General_Manager
+Orders
+"""
 
 ### TEST ###
-with open("/csv/test.csv") as file:
-    content = csv.reader(file)
-    content = [
-        {
-            'id': line[0],
-            'a': line[1],
-            'b': line[2],
-            'c': line[3],
-        }
-        for line in content if line
-    ]
+try:
+    with open("/csv/test.csv") as file:
+        content = csv.reader(file)
+        content = [
+            {
+                'id': line[0],
+                'a': line[1],
+                'b': line[2],
+                'c': line[3],
+            }
+            for line in content if line
+        ]
 
-    with sqlengine.begin() as transaction:
-        insert_stmt = sa.insert(tableT).values(content)
-        ret = transaction.execute(insert_stmt)
-    logging.info(f'Inserted {ret.rowcount} rows into table {tableT.name}')
+        with sqlengine.begin() as transaction:
+            insert_stmt = sa.insert(tableT).values(content)
+            ret = transaction.execute(insert_stmt)
+        logging.info(f'Inserted {ret.rowcount} rows into table {tableT.name}')
 
 
-### INSERT ###
-"""with open("/resources/.csv") as file:
-    content = csv.reader(file)
-    content = [
-        {
-            'id': line[0],
-            'a': line[1],
-            'b': line[2],
-            'c': line[3],
-        }
-        for line in content if line
-    ]
+    ### INSERT ###
+    for tableName in tables_ID_dict.keys():
 
-    with sqlengine.begin() as transaction:
-        sa.insert_stmt = sa.insert(table).values(content)
-        ret = transaction.execute(sa.insert_stmt)
-    logging.info(f'Inserted {ret.rowcount} rows into table {table.name}')
-"""
+        with open("/resources/regions.csv") as file:
+            content = csv.reader(file)
+            content = [
+                {
+                    'ID_region': autoIncrement(tableName),
+                    'Region_Name': line[0]
+                }
+                for line in content if line
+            ]
+
+            with sqlengine.begin() as transaction:
+                insert_stmt = sa.insert( tableName ).values(content)
+                ret = transaction.execute(insert_stmt)
+            logging.info(f'Inserted {ret.rowcount} rows into table { tableName.name }')
+        break 
+    # db_import    | [parameters: (1, 'DACH Region', 2, 'British Isles', 3, 'BeNeLux states', 4, 'Nordic Region', 5, 'CEE
+
+except:
+
